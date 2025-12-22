0c9c93286f6ef573a6ec76579ddcaae3464ef5f6
Jack Cordery <jack.redolf@gmail.com>
2025-12-22 09:23:28 +0000

    new bench tests

--- benchmarks/benches/my_benchmark.rs
+++ benchmarks/benches/my_benchmark.rs
@@ -1,38 +1,44 @@
+use ahash::AHashMap;
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use rand::Rng;
 use std::collections::HashMap;
 use std::hint::black_box;
 
-fn iterate_over_hmap(h: &HashMap<u8, bool>) -> u8 {
-    h.keys().copied().sum()
+fn iterate_over_hmap(h: &HashMap<u32, bool>) -> u32 {
+    h.keys().map(|e| e * 2).sum()
 }
 
-fn iterate_over_vec(v: &Vec<u8>) -> u8 {
-    v.iter().copied().sum()
+fn iterate_over_fmap(h: &AHashMap<u32, bool>) -> u32 {
+    h.keys().map(|e| e * 2).sum()
+}
+
+fn iterate_over_vec(v: &Vec<u32>) -> u32 {
+    v.iter().map(|e| e * 2).sum()
+}
+
+fn iterate_over_slice(v: &[u32]) -> u32 {
+    v.iter().map(|e| e * 2).sum()
 }
 
 fn bench_iteration(c: &mut Criterion) {
     let mut group = c.benchmark_group("iteration");
 
-    for &size in &[8usize, 32, 128, 1_024, 16_384] {
-        let vec: Vec<u8> = (0..size as u8).collect();
-        let hmap: HashMap<u8, bool> = vec.iter().copied().map(|x| (x, true)).collect();
+    let vec: Vec<u32> = (0..1_000_000).collect();
+    let hmap: HashMap<u32, bool> = vec.iter().copied().map(|x| (x, true)).collect();
+    let fmap: AHashMap<u32, bool> = vec.iter().copied().map(|x| (x, true)).collect();
 
-        group.bench_with_input(BenchmarkId::new("Vec", size), &size, |b, _| {
-            b.iter(|| {
-                // black_box the **result**
-                black_box(iterate_over_vec(black_box(&vec)))
-            })
-        });
-
-        group.bench_with_input(BenchmarkId::new("HashMap", size), &size, |b, _| {
-            b.iter(|| {
-                // black_box the **result**
-                black_box(iterate_over_hmap(black_box(&hmap)))
-            })
-        });
-    }
-
-    group.finish();
+    group.bench_with_input("vec", &vec, |b, i| {
+        b.iter(|| iterate_over_vec(black_box(i)))
+    });
+    group.bench_with_input("slice", &vec, |b, i| {
+        b.iter(|| iterate_over_slice(black_box(i)))
+    });
+    group.bench_with_input("hmap", &hmap, |b, i| {
+        b.iter(|| iterate_over_hmap(black_box(i)))
+    });
+    group.bench_with_input("fmap", &fmap, |b, i| {
+        b.iter(|| iterate_over_fmap(black_box(i)))
+    });
 }
 
 criterion_group!(benches, bench_iteration);

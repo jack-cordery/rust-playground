32fc94cccb04e67b19691663fe30a1abe94063c7
Jack Cordery <jack.redolf@gmail.com>
2025-12-24 12:01:39 +0000

    using generics in signature

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,6 +1,18 @@
+use std::fmt::Display;
+fn something<Something>(x: Something) -> impl Sync + Send + Display
+where
+    Something: Sync + Send + Display,
+{
+    x
+}
+
 fn main() {
     let x = 100;
     let y = &x;
+
+    let s = something(x);
+
+    let s_string = something("hello world");
 
     println!("{y:p}");
 
@@ -10,4 +22,6 @@ fn main() {
     println!("y is {y:p}");
     println!("y deref is {:p}", *y);
     println!("x is {x:p}");
+    println!("s is {s}");
+    println!("s_string is {s_string}");
 }

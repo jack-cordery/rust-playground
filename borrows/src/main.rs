81dba9c989a8ed3a2f4f6043f550abee4bcf0fa2
Jack Cordery <jack.redolf@gmail.com>
2026-01-08 09:51:11 +0000

    show how moving causes uninitialisation without copy

---
+++ borrows/src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Foo {
+    bar: u8,
+}
+
+fn main() {
+    // ok so this 'move' where y now owns x is valid
+    // the reason it works is that if T has copy then it will
+    // copy x and have its own independent value
+    // if it does not copy then x can no longer be called because
+    // it no longer owns anything and so is destroyed
+    let mut x: Foo = Foo { bar: 1 };
+    let y = x;
+
+    x = Foo { bar: x.bar + 1 };
+
+    println!("x:{x:?}");
+    println!("y:{y:?}");
+}
